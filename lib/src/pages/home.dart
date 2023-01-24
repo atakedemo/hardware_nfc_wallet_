406c8c2d1f00af606ac57141392a7e6d6bd98be7
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2023-01-24 22:53:04 +0900

    マイナンバー読み取りの追加

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,7 +1,33 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
+import 'package:nfc_manager/platform_tags.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  void readNFC() async {
+    print('Home is here');
+    await NfcManager.instance.startSession(
+      alertMessage: "ICカードをかざしてください",
+      onDiscovered: (NfcTag tag) async {
+        try {
+          final card = Iso7816.from(tag);
+          if (card == null) {
+            throw Exception("未サポートのICカードです");
+          }
+          // TODO: この辺で履歴とか読み取る
+        } catch (e, st) {
+          print(e);
+          print(st);
+          NfcManager.instance.stopSession(errorMessage: "読み取りに失敗しました");
+        }
+        NfcManager.instance.stopSession(alertMessage: "読み取り成功しました");
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +35,29 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ホーム'),
       ),
-      body:
+      body:Column(children: [
           const Center(child: Text('ホーム画面', style: TextStyle(fontSize: 32.0))),
+          const Center(child: Text('NFC読み取り', style: TextStyle(fontSize: 32.0))),
+          Center(
+          child: SingleChildScrollView(
+            child: ElevatedButton(
+              onPressed: () => readNFC(),
+              child: const Text("Touch NFC"),
+              style: ElevatedButton.styleFrom(
+                onPrimary: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ]),
+      /*
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          readNFC();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),*/ 
     );
   }
 }
--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:walletconnect_dart/walletconnect_dart.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,18 +14,33 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   late SessionStatus _session;
   late Uri _uri;
+  num _balance = 0;
 
   // connector を作成する関数
   WalletConnect createConnector() {
     return WalletConnect(
-        bridge: 'https://bridge.walletconnect.org',
-        clientMeta: const PeerMeta(
-            name: 'create_to_Metamask_App',
-            description: 'アプリの説明文です',
-            url: 'https://yamawo.info',
-            icons: [
-              'https://files.gitbook.com/v0/b/gitbook-legacy-files/o/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media'
-            ]));
+      bridge: 'https://bridge.walletconnect.org',
+      clientMeta: const PeerMeta(
+        name: 'create_to_Metamask_App',
+        description: 'アプリの説明文です',
+        url: 'https://bamb00.info',
+        icons: [
+          'https://files.gitbook.com/v0/b/gitbook-legacy-files/o/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media'
+        ]
+      )
+    );
+  }
+
+  void getBalance(String address) async {
+    var apiUrl = "https://eth-mainnet.g.alchemy.com/v2/r6zSubRCOAPzLimDRt_R0MzIOx7EVsEm";
+    var httpClient = new Client();
+    var ethClient = new Web3Client(apiUrl, httpClient);
+
+    EthereumAddress addr = EthereumAddress.fromHex(address);
+    EtherAmount balance = await ethClient.getBalance(addr);
+    print(addr);
+    print(balance);
+    _balance = await balance.getValueInUnit(EtherUnit.ether);
   }
 
   // ボタンが押された際に発火させる、walletConnect との session を確立する関数
@@ -34,7 +51,7 @@ class _LoginPageState extends State<LoginPage> {
     if (!connector.connected) {
       try {
         final session =
-            await connector.createSession(onDisplayUri: (uri) async {
+          await connector.createSession(onDisplayUri: (uri) async {
           _uri = Uri.parse(uri);
           // Metamask アプリを立ち上げる
           await launchUrl(Uri.parse(uri));
@@ -46,6 +63,11 @@ class _LoginPageState extends State<LoginPage> {
       } catch (e) {
         print(e);
       }
+    } else {
+      // sessionに対して処理を設置する
+      connector.on('connect', (session) => print(session));
+      connector.on('session_update', (payload) => print(payload));
+      connector.on('disconnect', (session) => print(session));
     }
   }
 
@@ -55,17 +77,37 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: const Text('アカウント設定'),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: ElevatedButton(
-            onPressed: () => connectToMetamask(),
-            child: const Text("メタマスクと連携する"),
-            style: ElevatedButton.styleFrom(
-              onPrimary: Colors.black,
+      body: Column(children: [
+        Center(
+          child: SingleChildScrollView(
+            child: ElevatedButton(
+              onPressed: () => connectToMetamask(),
+              child: const Text("メタマスクと連携する"),
+              style: ElevatedButton.styleFrom(
+                onPrimary: Colors.white,
+              ),
             ),
           ),
         ),
-      ),
+        Text('残高を取得する'),
+        Center(
+          child: SingleChildScrollView(
+            child: ElevatedButton(
+              onPressed: () => getBalance("0xd8da6bf26964af9d7eed9e03e53415d37aa96045"),
+              child: const Text("ETH残高を取得する"),
+              style: ElevatedButton.styleFrom(
+                onPrimary: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Text('ETH 残高：'),
+        Text(
+          '$_balance',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ])
+      
     );
   }
 }
